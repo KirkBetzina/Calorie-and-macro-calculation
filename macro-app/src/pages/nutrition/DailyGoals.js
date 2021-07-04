e2f3c666963f2b85cf211f7715def24c9316f1e4
kirk <kirk.betzina@gmail.com>
2021-07-04 17:48:06 -0400

    doing basic setups

---
+++ macro-app/src/pages/nutrition/DailyGoals.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const DailyGoal = (props) => {
+    return (
+        <div>
+            <h1>This is where all the daily goals for macros and calories, and will count down and show a cirlce graph, this will update weekly totals.</h1>
+        </div>
+    )
+}
+export default DailyGoal
