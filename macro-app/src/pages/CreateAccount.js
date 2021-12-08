5506f3a3a04d66e1347228c03ff4b5f67ae9933f
kirk <kirk.betzina@gmail.com>
2021-12-08 09:13:45 -0500

    added the create account form component to page

--- macro-app/src/pages/CreateAccount.js
+++ macro-app/src/pages/CreateAccount.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { Component } from 'react'
+import CreateAccountForm from '../Component/CreateAccountForm'
 
 const CreateAccount = (props) => {
     return(
         <div>
+            <CreateAccountForm />
             <h1>This is the creat account form this is where ther user will create account credentials, plug in bmr info to lock that into the users account.</h1>
         </div>
     )
