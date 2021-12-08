9edaf5047dc4f77c81f2c718fdaf657cebf12b6b
kirk <kirk.betzina@gmail.com>
2021-12-08 12:58:49 -0500

    setting up splash page on splash branch

--- macro-app/src/App.js
+++ macro-app/src/App.js
@@ -1,12 +1,14 @@
 import BmrCalculator from './components/BmrCalculator';
 import CreateAccountForm from './components/CreateAccountForm';
 import './App.css';
+import SplashPage from './pages/SplashPage';
 
 function App() {
   return (
     <div className="App">
-    <CreateAccountForm />
-    <BmrCalculator />
+      <SplashPage />
+    {/* <CreateAccountForm />
+    <BmrCalculator /> */}
     </div>
   );
 }
--- macro-app/src/pages/SplashPage.js
+++ macro-app/src/pages/SplashPage.js
@@ -1,3 +1,4 @@
+import { Button } from 'reactstrap'
 import React from 'react'
 
 const SplashPage = (props) => {
@@ -5,7 +6,10 @@ const SplashPage = (props) => {
         <div className='splashPage'>
             <h1>The fitness industry will HATE this App</h1>  
             <p>This is desinged for every person at every level of fitness. From the beginner just making their first steps in getting control of their fitness, or the person that has been looking for a better way to automate their diet to achieve their goals.</p>
-            <p>How it works: Input your information into our form, it will calculate your BMR and then it will give you a few options to choose from. Caloric deficit, maintenance, or caloric surplus. These dietary guidlines are designed specifically for you and will only work for you since its based off your own Base Metabolic Rate.</p>          
+            <p>How it works: Input your information into our form, it will calculate your BMR and then it will give you a few options to choose from. Caloric deficit, maintenance, or caloric surplus. These dietary guidlines are designed specifically for you and will only work for you since its based off your own Base Metabolic Rate.</p>   
+
+            <Button color="danger" size="lg">Sign Up Now</Button>{''} 
+            
         </div>
     )
 }
