998d60dd25e6fb632b9ee4a81cc31fb2976b8a42
kirk <kirk.betzina@gmail.com>
2022-01-01 20:52:33 -0500

    added business logic but need to change for specific project.

--- macro-app/src/App.js
+++ macro-app/src/App.js
@@ -4,6 +4,27 @@ import './App.css';
 import SplashPage from './pages/SplashPage';
 
 function App() {
+  const getLogin = (username, password) => {
+    fetch(url + '/wallets/login/' + username + '/' + password)
+    .then((response) => response.json())
+    .then((data) => {
+      setUser(data);
+      console.log('HEY OVER HERE IM CONSOLE LOGGIN DATA', data)
+      if (data.status === 200)
+      
+      {
+        setUser(data.data.wallet._id)
+        props.history.push('/home')
+      } else if (data.status === 409) {
+        alert('username does not exist')
+        props.history.push('/create')
+      } else if (data.status === 403) {
+        alert('username or password is WRONG!')
+      }
+      
+
+    })
+  }
   
   return (
     <div className="App">
